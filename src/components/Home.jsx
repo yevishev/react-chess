d3064931229ff239d7101cf10cbe8b2c89a0a939
yevishev <yevishev.m@gmail.com>
2023-09-14 11:10:53 +0300

    init react

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Home.module.css';
+
+export default () => {
+    return (
+        <h1 className={styles.block}>Hi!</h1>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 
+import Home from './components/Home';
 const container = document.getElementById('app');
 const root = createRoot(container);
 
